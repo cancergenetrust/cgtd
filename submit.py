eb92b2e5dc99fb289fb928d14b63d2d91781d59f
Rob Currie <rcurrie@ucsc.edu>
2017-10-03 01:48:53 +0000

    Move back to last ipfs version due to harmless but annoying dht error, fix debug reload location to serve app from /app instead of /usr, add utility submit.py to add submission generated by the submit.cancergenetrust.org single page web app supporting ucsf cnext cancer registrar and foundation one

---
+++ submit.py
@@ -0,0 +1,21 @@
+import json
+import requests
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Upload submission from submit.cancergenetrust.org")
+parser.add_argument('file', nargs='?', default="submission.json",
+                    help="Path to json file to submit")
+args = parser.parse_args()
+
+with open(args.file) as f:
+    submission = json.loads(f.read())
+submission["clinical"]["CGT Public ID"] = submission["patientId"]
+
+r = requests.post("http://localhost:5000/v0/submissions?publish=true",
+                  files=[("files[]",
+                          ("foundationone.json",
+                           json.dumps(submission["genomic"], sort_keys=True)))],
+                  data=submission["clinical"])
+print(r.text)
+assert(r.status_code == requests.codes.ok)
